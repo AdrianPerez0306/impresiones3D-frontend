8e20c79c33e43aca910ba9179ad94e5af5218714
diegolentz <diegoolentz@gmail.com>
2025-01-06 15:32:39 -0300

    footer size

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,10 +2,12 @@ import './footer.css'
 
 export const Footer = () => {
     return (
+        <div className='contenedor'>
         <footer>
             <span>Social 1</span>
             <span>Social 2</span>
             <span>Social 3</span>
         </footer>
+        </div>
     )
 }
