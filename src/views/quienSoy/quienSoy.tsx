39ffe802dbf2a91638e985be0392473ace4e02f8
diegolentz <diegoolentz@gmail.com>
2025-01-06 14:54:32 -0300

    estructurando

---
+++ src/views/quienSoy/quienSoy.tsx
@@ -0,0 +1,44 @@
+import { aboutMe } from '../../models/module';
+import './quienSoy.css'
+export const QuienSoy = () => {
+
+    return <>
+        <div className="aboutMe">
+            <h2>V-Laboratory</h2>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+            <span>{aboutMe}</span>
+          
+           
+        </div>
+  
+
+    </>
+};
+
+
