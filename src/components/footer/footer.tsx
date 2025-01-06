46987c2d6a45221a61cf1da62b112b4cf7fa4d21
diegolentz <diegoolentz@gmail.com>
2025-01-06 17:38:29 -0300

    quien soy cocinado

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,12 +2,18 @@ import './footer.css'
 
 export const Footer = () => {
     return (
-        <div >
-        <footer>
-            <span>Social 1</span>
-            <span>Social 2</span>
-            <span>Social 3</span>
-        </footer>
+        <div>
+            <footer>
+                <div className="redes">
+                        <img src="https://img.icons8.com/ios/50/000000/instagram-new--v1.png" alt="instagram"/>
+                        <img src="https://img.icons8.com/ios/50/000000/facebook--v1.png" alt="facebook"/>
+                        <img src="https://img.icons8.com/ios/50/000000/twitter--v1.png" alt="twitter"/>
+                        <img src="https://img.icons8.com/ios/50/000000/gmail.png" alt="gmail"/>
+                        <img src="https://img.icons8.com/ios/50/000000/whatsapp.png" alt="whatsapp"/>
+                        <img src="https://img.icons8.com/ios/50/000000/discord-logo.png" alt="discord"/>
+                </div>
+              
+            </footer>
         </div>
     )
 }
