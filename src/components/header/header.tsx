9682ebfa461c227be26b63978651144309700a98
diegolentz <diegoolentz@gmail.com>
2025-01-16 00:22:30 -0300

    ok

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,8 +4,8 @@ export const Header = () => {
     return (
         <header>
             <div className="content">
-                <img src="./src/assets/logo.jpeg" alt="IMG" className='logo'  />
-                 <img src="./src/assets/title.jpeg" alt="TITLE" className='titulo' />
+                <img src="../src/assets/logo.jpeg" alt="IMG" className='logo'  />
+                 <img src="../src/assets/title.jpeg" alt="TITLE" className='titulo' />
             </div>
         </header>
     )
