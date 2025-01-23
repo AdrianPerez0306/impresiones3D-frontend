7484b1327a3a109b565f01d84d187f9c54ff70e2
diegolentz <diegoolentz@gmail.com>
2025-01-23 19:28:53 -0300

    header nuevo

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -4,7 +4,6 @@ export const Header = () => {
     return (
         <header>
             <div className="content">
-                <img src="../src/assets/logo.jpeg" alt="IMG" className='logo'  />
                  <img src="../src/assets/title.jpeg" alt="TITLE" className='titulo' />
             </div>
         </header>
