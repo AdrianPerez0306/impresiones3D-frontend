d9769a4223b8a38b40df67a6e30f02d48a83b512
diegolentz <diegoolentz@gmail.com>
2025-02-07 18:28:24 -0300

    se soluciona los 2 useEffect del prductodetail

--- src/views/productDetail/productDetail.tsx
+++ src/views/productDetail/productDetail.tsx
@@ -7,116 +7,75 @@ import { Articulo } from "../../models/Articulo";
 import { productService } from "../../service/product.service";
 import { IColor } from "../../models/Color";
 
-
 export const ProductDetail = () => {
-    const routeParameter = useParams()
-    const [product, setProduct] = useState<Articulo>()
-    const [coloresList, setColoresList] = useState<IColor[]>([])
-    const [imagenesList, setImagenesList] = useState<string[]>([])
-    const [imagenView, setimagenView] = useState<string>()
-    const [indexImage, setIndexImage] = useState<number>(0)
-    const navigate = useNavigate()
+    const { id } = useParams();
+    const [product, setProduct] = useState<Articulo | null>(null);
+    const [coloresList, setColoresList] = useState<IColor[]>([]);
+    const [imagenesList, setImagenesList] = useState<string[]>([]);
+    const [imagenView, setImagenView] = useState<string | undefined>();
+    const [indexImage, setIndexImage] = useState<number>(0);
+    const navigate = useNavigate();
 
-    const fetchData = async () => {
-        const res = await productService.getProduct(Number(routeParameter.id))
-        setProduct(res)
-        obtenerColores()
-        obtenerImagenes()
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const res = await productService.getProduct(Number(id));
+                setProduct(res);
+                setColoresList(res.colores);
 
-    const obtenerColores = () => {
-        if (product?.colores) {
-            setColoresList(product.colores);
-        }
-    };
+                const img = [res.imagen_1, res.imagen_2, res.imagen_3, res.imagen_4, res.imagen_5].filter(image => image !== null );
+                setImagenesList(img);
+                setImagenView(img[0]);
+                setIndexImage(0);
+            } catch (error) {
+                console.error("No se pudo obtener el producto:", error);
+            }
+        };
 
-
-    const obtenerImagenes = () => {
-        if (product) {
-            const img = [product.imagen_1, product.imagen_2, product.imagen_3, product.imagen_4, product.imagen_5];
-            setImagenesList(img.filter((imagen) => imagen));
-            setimagenView(img[0]);
-            setIndexImage(imagenesList.length);
-        }
-    };
+        fetchData();
+    }, []); 
 
     const nextImage = () => {
-        if (indexImage < imagenesList.length - 1) {
-            setimagenView(imagenesList[indexImage + 1]);
-            setIndexImage(indexImage + 1);
-        } else {
-            setimagenView(imagenesList[0]);
-            setIndexImage(0);
-        }
+        const newIndex = (indexImage + 1) % imagenesList.length;
+        setIndexImage(newIndex);
+        setImagenView(imagenesList[newIndex]);
     };
 
     const previousImage = () => {
-        if (indexImage > 0) {
-            setimagenView(imagenesList[indexImage - 1]);
-            setIndexImage(indexImage - 1);
-        } else {
-            setimagenView(imagenesList[imagenesList.length - 1]);
-            setIndexImage(imagenesList.length - 1);
-        }
+        const newIndex = (indexImage - 1 + imagenesList.length) % imagenesList.length;
+        setIndexImage(newIndex);
+        setImagenView(imagenesList[newIndex]);
     };
 
-    useEffect(() => {
-        fetchData();
-    }, []);
-
-    useEffect(() => {
-        if (product) {
-            obtenerColores();
-            obtenerImagenes();
-        }
-    }, [product]);
-
-
-    function agregarVolver(): () => void {
-        return () => {
-            console.log("Product added to cart");
-            navigate(`/home`);
-        };
-    }
-
-    function volverProductos(): () => void {
-        return () => {
-            navigate(`/home`);
-        };
-    }
-
-    return <>
-        <div className="containerDetalleProducto">
-            <div className="img1">
-                <img className="imagen1" src={imagenView} />
+    return (
+        <>
+            <div className="containerDetalleProducto">
+                <div className="img1">
+                    <img className="imagen1" src={imagenView} alt="Producto" />
+                </div>
+                <div className="descripcion">
+                    <h5>Descripción</h5>
+                    <p>{product?.detalle}</p>
+                </div>
+                <div className="colores">
+                    <h4>Selecciona un color</h4>
+                    <select name="selectColors" className="selectColor">
+                        {coloresList.map((color, index) => (
+                            <option key={index} value={color.nombre}>
+                                {color.nombre}
+                            </option>
+                        ))}
+                    </select>
+                </div>
+                <div className="control">
+                    <button onClick={previousImage}><p>Anterior</p></button>
+                    <button onClick={nextImage}><p>Siguiente</p></button>
+                </div>
             </div>
-            <div className="descripcion">
-                <h5>Descripcion</h5>
-                <p>{product?.detalle}</p>
+            <div className="guardarCancelar">
+                <ButtonRed label="Volver" onClick={() => navigate("/home")} />
+                <ButtonGreen label="Agregar y seguir comprando" onClick={() => navigate("/home")} />
             </div>
-            <div className="colores">
-                <h4>Selecciona un color</h4>
-                <select name="selectColors" className="selectColor" >
-                    {coloresList.map((color, index) => (
-                        <option key={index} value={color.nombre}>
-                            {color.nombre}
-                        </option>
-                    ))}
-                </select>
-            </div>
-            <div className="control">
-                <button onClick={previousImage}> <p>Anterior</p> </button>
-                <button onClick={nextImage}> <p>Siguiente</p></button>
-            </div>
-        </div>
-        <div className="guardarCancelar">
-            <div>
-                <ButtonRed label="Volver" onClick={volverProductos()}></ButtonRed>
-            </div>
-            <div>
-                <ButtonGreen label="Agregar y seguir comprando" onClick={agregarVolver()}></ButtonGreen>
-            </div>
-        </div>
-    </>
+        </>
+    );
 };
-
