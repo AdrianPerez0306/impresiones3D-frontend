8a807f3934fad203e4442021d5c6873b56f1ee53
diegolentz <diegoolentz@gmail.com>
2025-02-11 21:11:33 -0300

    qedando, implementar logica, mejorar un poco css

--- src/views/cart/cart.tsx
+++ src/views/cart/cart.tsx
@@ -4,53 +4,61 @@ import "./cart.css"; // Archivo de estilos
 
 const CartComponent = () => {
     const articuloUser = useSelector((state: RootState) => state.cart);
+    
 
     return (
         <div className="cartContainer">
             {articuloUser.length === 0 ? (
                 <p>El carrito está vacío</p>
             ) : (
-                <div>
-                    <table className="cartTable">
-                        <thead >
-                            <tr className="cartTableHeader">
-                                <th>Articulo</th>
-                                <th>Cantidad</th>
-                                <th>Eliminar</th>
-                                <th>Precio</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {articuloUser.map((item, index) => (
-                                <tr key={index}>
-                                    <td className="imgCart">
-                                        <div className="articulo">
-                                            {item.titulo}
-                                            <img src={item.imagen} alt={item.titulo} />
-                                            {item.color}
-                                            {item.dimension_mm}
-                                        </div>
-                                    </td>
-                                    <td className="cantidad">
-                                        <div className="contador">
-                                            <button>-</button>
-                                            {item.cantidad}
-                                            <button>+</button>
-                                        </div>
-                                    </td>
-                                    <td className="eliminar" >
-                                        <button>Eliminar</button>
-                                    </td>
-                                    <td className="precio">${item.precio_lista}</td>
+                <>
+                    <div>
+                        <table className="cartTable">
+                            <thead>
+                                <tr className="cartTableHeader">
+                                    <th>Articulo</th>
+                                    <th>Cantidad</th>
+                                    <th>Eliminar</th>
+                                    <th>Precio</th>
                                 </tr>
-                            ))}
-                            <tr >
-                                <td className="precioFinal" colSpan={3}>Precio Final</td>
-                                <td className="precioFinal">.....</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
+                            </thead>
+                            <tbody>
+                                {articuloUser.map((item, index) => (
+                                    <tr key={index}>
+                                        <td className="imgCart">
+                                            <div className="articulo">
+                                                {item.titulo}
+                                                <img src={item.imagen} alt={item.titulo} />
+                                                {item.color}
+                                                {item.dimension_mm}
+                                            </div>
+                                        </td>
+                                        <td className="cantidad">
+                                            <div className="contador">
+                                                <button>-</button>
+                                                {item.cantidad}
+                                                <button>+</button>
+                                            </div>
+                                        </td>
+                                        <td className="eliminar">
+                                            <button>X</button>
+                                        </td>
+                                        <td className="precio">${item.precio_lista}</td>
+                                    </tr>
+                                ))}
+                                <tr>
+                                    <td className="precioFinal" colSpan={3}>Precio Final</td>
+                                    <td className="precioFinal">.....</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <div className="confirmacion">
+                        <h3>Introduce un numero de celular y nos contactaremos a la brevedad</h3>
+                        <input type="number" />
+                    </div>
+                </>
             )}
         </div>
     );
