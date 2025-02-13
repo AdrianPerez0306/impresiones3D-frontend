79068a7e0855d25f255f398360995851fd3bf2bc
diegolentz <diegoolentz@gmail.com>
2025-02-13 20:29:27 -0300

    ahora si quedo el parseo

---
+++ src/components/productDetail/detail.tsx
@@ -0,0 +1,33 @@
+export const ProductDetail = () => {
+
+
+
+//     <div className="descripcion">
+//     <h5>Descripción</h5>
+//     <p>{product?.detalle}</p>
+// </div>
+
+// <div className="colores">
+//     <h4>Selecciona un color</h4>
+//     <select name="selectColors" className="selectColor">
+//         {coloresList.map((color, index) => (
+//             <option key={index} value={color.nombre}>
+//                 {color.nombre}
+//             </option>
+//         ))}
+//     </select>
+// </div>
+
+// <div className="medidas">
+//     <p>Selecciona tamaño</p>
+//     <h5>checks de tamaños</h5>
+// </div>
+// </div>
+// <div className="guardarCancelar">
+// <ButtonRed label="Volver" onClick={() => navigate("/home")} />
+// <ButtonGreen label="Añadir" onClick={agregarAlChango} />
+// </div>
+
+
+
+}
--- src/service/product.service.ts
+++ src/service/product.service.ts
@@ -5,15 +5,28 @@ class ProductService {
     constructor(){}
 
     async getAllProduct() : Promise<Articulo[]>{
-        return (await axios.get<Articulo[]>('http://localhost:8080/articulos')).data 
+        return (await axios.get<Articulo[]>('http://localhost:8080/articulos')).data;
     }
 
     async getProduct(id : number) : Promise<ArticuloDetalle>{
-        const res = await axios.get<ArticuloDetalle>(`http://localhost:8080/articulos/${id}`)
-        const item = res.data
-        return new ArticuloDetalle(item.id, item.titulo, item.detalle, item.precio_lista, item.descuento, item.categorias, item.colores, item.dimensiones_mm as unknown as IDimension[], item.imagenes)
+        const res = await axios.get(`http://localhost:8080/articulos/${id}`);
+        const item = res.data;
+        const dimensiones: string[] = item.dimensiones_mm.map((dim: IDimension) => {
+            return `${dim.alto_mm} X ${dim.ancho_mm} X ${dim.profundidad_mm}`;
+        });
+
+        return new ArticuloDetalle(
+            item.id, 
+            item.titulo, 
+            item.detalle, 
+            item.precio_lista, 
+            item.descuento, 
+            item.categorias, 
+            item.colores,
+            dimensiones, 
+            item.imagenes
+        );
     }
 }
 
-export const productService = new ProductService()
-
+export const productService = new ProductService();
