af8e76c591dad08ccaa54bc47d375d24ef2a4399
diegolentz <diegoolentz@gmail.com>
2025-02-24 17:17:25 -0300

    agregando input mail

--- src/views/cart/cart.tsx
+++ src/views/cart/cart.tsx
@@ -8,33 +8,37 @@ const CartComponent = () => {
     const articuloUser = useSelector((state: RootState) => state.cart);
     const dispatch = useDispatch();
     const [precioTotal, setprecioTotal] = useState(0);
-    
-    
+    const [mail, setMail] = useState("");
+
+
     const eliminarArticulo = (index: number) => {
         dispatch(removeFromCart(index));
     };
-    
+
     const sumar = (index: number) => {
         dispatch(updateCantidad({ index, cantidad: articuloUser[index].cantidad + 1 }));
     };
     const restar = (index: number) => {
         dispatch(updateCantidad({ index, cantidad: articuloUser[index].cantidad - 1 }));
-        
+
     }
 
-    
+
     useEffect(() => {
         var total = 0;
         articuloUser.forEach((item) => {
             total += item.precio_lista * item.cantidad;
         });
         setprecioTotal(total);
-    }, [articuloUser]);    
+    }, [articuloUser]);
 
     return (
         <div className="cartContainer">
             {articuloUser.length === 0 ? (
-                <p>El carrito está vacío</p>
+                <div className="vacio">
+                    <p>El carrito se encuentra vacío, te invito a navegar la pagina y agregar aquellos articulos de tu interes.</p>
+                    <a href="/productos">Ver Productos</a>
+                </div>
             ) : (
                 <>
                     <div>
@@ -60,14 +64,14 @@ const CartComponent = () => {
                                         </td>
                                         <td className="cantidad">
                                             <div className="contador">
-                                                <button className="contadorRestar" onClick={() =>restar(index)}>-</button>
+                                                <button className="contadorRestar" onClick={() => restar(index)}>-</button>
                                                 {item.cantidad}
-                                                <button className="contadorSumar" onClick={() =>sumar(index)}>+</button>
+                                                <button className="contadorSumar" onClick={() => sumar(index)}>+</button>
                                             </div>
                                         </td>
                                         <td className="precio">${item.precio_lista}</td>
                                         <td className="eliminar">
-                                            <button className="eliminarProducto"onClick={ ()=> eliminarArticulo(index)}>X</button>
+                                            <button className="eliminarProducto" onClick={() => eliminarArticulo(index)}>X</button>
                                         </td>
                                     </tr>
                                 ))}
@@ -75,14 +79,18 @@ const CartComponent = () => {
                                     <td className="precioFinal" colSpan={3}>Precio Final</td>
                                     <td className="precioFinal">${precioTotal}</td>
                                 </tr>
+                                <tr>
+                                    <td className="datosMail"colSpan={4}>
+                                        <p>Introduce un e-Mail y nos contactaremos a la brevedad</p>
+                                        <input className="inputMail" type="email" placeholder="Introduce tu e-Mail" />
+                                    </td>
+                                </tr>
+                               
                             </tbody>
                         </table>
                     </div>
 
-                    <div className="confirmacion">
-                        <h3>Introduce un numero de celular y nos contactaremos a la brevedad</h3>
-                        <input type="number" />
-                    </div>
+
                 </>
             )}
         </div>
